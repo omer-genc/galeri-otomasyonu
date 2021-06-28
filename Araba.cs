fadc870452e738f098c9bb3190d39c66b0dfc145
omer genc <omergnc1@gmail.com>
2021-06-28 23:09:17 +0300

    draft created

--- Araba.cs
+++ Araba.cs
@@ -1,9 +1,66 @@
 using System;
+using System.Collections.Generic;
+using System.Text;
 
 namespace galeri_otomasyonu
 {
     class Araba
     {
+        public string Plaka { get; set; }
+        public string Marka { get; set; }
+        public float KiralamaBedeli { get; set; }
+
+        public List<int> KiralanmaSureleri = new List<int>();
+
+        public int ToplamKiralanmaSuresi
+        {
+            get
+            {
+                int toplam = 0;
+                foreach (int x in this.KiralanmaSureleri)
+                {
+                    toplam += x;
+                }
+                return toplam;
+            }
+        }
+        public int KiralanmaSayisi
+        {
+            get
+            {
+                return this.KiralanmaSureleri.Count;
+            }
+        }
+
+        public ARAC_TIPI AracTipi { get; set; }
+        public DURUM Durum { get; set; }
+
+
+        public Araba(string plaka, string marka, float kiralamaBedeli, ARAC_TIPI aracTipi)
+        {
+            //burada atamayı yaparken tüm harfleri büyüterek atayalım.
+            this.Plaka = plaka.ToUpper();
+            this.Marka = marka;
+            this.KiralamaBedeli = kiralamaBedeli;
+            this.AracTipi = aracTipi;
+            this.Durum = DURUM.Galeride;
+        }
         
+    } // end class
+
+     public enum DURUM
+    {
+        Empty = 45,
+        Kirada = 65,
+        Galeride = 74
     }
+
+    public enum ARAC_TIPI
+    {
+        Empty = 34,
+        SUV = 56,
+        Sedan = 23,
+        Hatcback = 76
+    }
+
 }
--- Galeri.cs
+++ Galeri.cs
@@ -1,9 +1,59 @@
 using System;
-
+using System.Collections.Generic;
+using System.Text;
 namespace galeri_otomasyonu
 {
     class Galeri
     {
+        public List<Araba> Arabalar = new List<Araba>();
+
+        public int ToplamAracSayisi { get; }
+        public int KiradakiAracSayisi
+        {
+            get
+            {
+                int adet = 0;
+
+                foreach (Araba item in this.Arabalar)
+                {
+                    if (item.Durum == DURUM.Kirada)
+                    {
+                        adet++;
+                    }
+                }
+
+                return adet;
+            }
+        }
+        public int GaleridekiAracSayisi { get; }
+        public int ToplamAracKiralanmaSuresi { get; }
+        public int ToplamAracKiralanmaAdedi { get; }
+        public int Ciro { get; }
+
+
+
+        public void ArabaKirala(string plaka, int sure)
+        {
+            Araba a = null;
+
+            foreach (Araba item in this.Arabalar)
+            {
+                if (item.Plaka == plaka)
+                {
+                    a = item;
+                }
+            }
+
+            a.KiralanmaSureleri.Add(sure);
+            a.Durum = DURUM.Kirada;
+        }
+
+
+        public void ArabaEkle()
+        {
+            //bu metodun parametresinden arabaya ait tüm bilgiler tek tek gelmeli
+
+        }
         
     }
 }
