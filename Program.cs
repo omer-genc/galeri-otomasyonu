3b9a7d7996f995ef31691ead3125c8b98a8b9d66
omer genc <omergnc1@gmail.com>
2021-06-28 22:47:22 +0300

    projec created

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace galeri_otomasyonu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
